156ab60774a61770cf35d7031b11c3e3df0706aa
Kelton Person <kelton.person@gmail.com>
2015-02-22 08:29:36 -0600

    update to 0.13.7

--- project/build.scala
+++ project/build.scala
@@ -6,8 +6,7 @@ object ScalaCache extends Build {
   val Organization = "com.cache"
   val Name = "Cache"
   val Version = "0.1.0-SNAPSHOT"
-  val ScalaVersion = "2.11.1"
-  val ScalatraVersion = "2.3.0"
+  val ScalaVersion = "2.11.5"
 
   lazy val cache = Project (
     "cache",
@@ -17,6 +16,7 @@ object ScalaCache extends Build {
       name := "cache",
       version := Version,
       scalaVersion := ScalaVersion,
+      ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
       resolvers += Classpaths.typesafeReleases,
       libraryDependencies ++= Seq(
       )
